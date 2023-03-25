043ef067d9fb73eeb8e22db6a544898af95e6558
Orestli <orestlit1234@gmail.com>
2023-03-26 00:24:46 +0200

    fix(sign-up): scroll

--- src/components/auth-layout/index.tsx
+++ src/components/auth-layout/index.tsx
@@ -7,12 +7,12 @@ interface Props {
 
 const AuthLayout: React.FC<Props> = ({ imageSrc, children }) => {
   return (
-    <main className="w-full flex">
-      <div className="w-1/3 hidden h-screen lg:block">
+    <main className="flex">
+      <div className="relative w-1/3 hidden h-full lg:block">
         <Image
           src={imageSrc}
           alt="background-image"
-          className="w-full h-screen object-cover"
+          className="absolute w-full h-screen object-cover"
         />
       </div>
       <div className="w-full max-w-[600px] xxl:max-w-[760px] h-screen mx-auto px-5 flex flex-col flex-grow">
