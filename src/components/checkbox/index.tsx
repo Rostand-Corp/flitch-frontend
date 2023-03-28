181976c4acedfc8aae593a711a209340aff6c135
Roman Kryzhanivsky <r.krijaniv@gmail.com>
2023-03-28 21:50:15 +0300

    refactor: the 'check' svg saved in /public/icons/ and rendered as a component.

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,5 +1,7 @@
 import { memo } from 'react';
+import { twMerge } from 'tailwind-merge';
 
+import Check from '@public/images/check.svg';
 import { RequiredKeys } from '@typings/index';
 
 type InputProps = RequiredKeys<
@@ -10,26 +12,16 @@ type InputProps = RequiredKeys<
   'id'
 >;
 
-const Checkbox: React.FC<InputProps> = ({ ...props }) => (
+const Checkbox: React.FC<InputProps> = ({ className, ...props }) => (
   <label
-    className="w-7 w-7 flex justify-center items-center rounded-lg border-2 
-  border-solid border-black border-gray-600 cursor-pointer overflow-hidden"
+    className={twMerge(
+      'w-7 w-7 flex justify-center items-center rounded-lg border-2 border-solid ' +
+        'border-black border-gray-600 cursor-pointer overflow-hidden',
+      className,
+    )}
   >
     <input type="checkbox" {...props} className="peer hidden" />
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="2.5"
-      stroke="currentColor"
-      className="w-7 w-7 invisible peer-checked:visible"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M4.5 12.75l6 6 9-13.5"
-      />
-    </svg>
+    <Check className="w-7 w-7 invisible peer-checked:visible" />
   </label>
 );
 
