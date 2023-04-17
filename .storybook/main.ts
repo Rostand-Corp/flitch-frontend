d0e25cef7384ec135a12c702b81e3dd51e5bf00b
Orestli <orestlit1234@gmail.com>
2023-04-17 18:57:41 +0300

    feat: update storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,24 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-addon-mock/register',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -13,6 +26,32 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+  },
+  webpackFinal: (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config?.resolve?.alias,
+        '@api': path.join(__dirname, 'src/api'),
+        '@components': path.join(__dirname, 'src/components'),
+        '@config': path.join(__dirname, 'src/config'),
+        '@hooks': path.join(__dirname, 'src/hooks'),
+        '@layouts': path.join(__dirname, 'src/layouts'),
+        '@pages': path.join(__dirname, 'src/pages'),
+        '@public': path.join(__dirname, './public'),
+        '@store': path.join(__dirname, 'src/store'),
+        '@styles': path.join(__dirname, 'src/styles'),
+        '@typings': path.join(__dirname, 'src/typings'),
+        '@utils': path.join(__dirname, 'src/utils'),
+      };
+    }
+
+    config.module?.rules?.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   },
 };
 export default config;
---
+++ src/stories/components/input.stories.tsx
@@ -0,0 +1,15 @@
+import type { StoryObj, StoryFn } from '@storybook/react';
+
+import Input from '@components/input';
+
+export default {
+  title: 'components/Input',
+  component: Input,
+} as StoryObj<typeof Input>;
+
+const Template: StoryFn<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  placeholder: 'Placeholder...',
+};
