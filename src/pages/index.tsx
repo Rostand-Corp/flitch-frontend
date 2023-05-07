711faa6d76874b1ad1d57ca861e68c661a9ec4b7
Roman Kryzhanivsky <r.krijaniv@gmail.com>
2023-05-07 20:28:31 +0300

    fix: index.tsx page modified

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,17 @@
 import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-import Button from '@components/button';
-import routes from '@config/routes';
+import Home from '@layouts/home';
 
-const Home: NextPage = () => {
-  const router = useRouter();
-
+const HomePage: NextPage = () => {
   return (
-    <div className="max-w-[200px] flex flex-col gap-1">
-      <Button appearance="primary" onClick={() => router.push(routes.signIn)}>
-        Sign In
-      </Button>
-      <Button appearance="primary" onClick={() => router.push(routes.signUp)}>
-        Sign Up
-      </Button>
-    </div>
+    <>
+      <Head>
+        <title>Sign In - Flitch</title>
+      </Head>
+      <Home />
+    </>
   );
 };
 
-export default Home;
+export default HomePage;
