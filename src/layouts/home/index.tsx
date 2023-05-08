9807e2d67fbd535fa9b6650de787e977fb21ff0b
Roman Kryzhanivsky <r.krijaniv@gmail.com>
2023-05-08 17:41:30 +0300

    refactor: after review

--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -23,35 +23,28 @@ const Home: React.FC = () => {
   );
 
   return (
-    <main className="w-full min-h-screen flex">
-      <div
-        className={twMerge(
-          'transition-all duration-500 ease-in-out overflow-hidden lg:basis-2/6',
-          !contentIsVisible
-            ? 'basis-full xs:basis-2/3 sm:basis-2/4 md:basis-2/5'
-            : 'basis-0',
-        )}
-      >
+    <main
+      className={twMerge(
+        'w-full min-h-screen grid transition-all duration-500' +
+          'lg:grid-cols-[30%_70%] xl:grid-cols-[25%_75%]',
+        contentIsVisible
+          ? 'grid-cols-[0%_100%] xs:grid-cols-[0%_100%] sm:grid-cols-[0%_100%] md:grid-cols-[0%_100%]'
+          : 'grid-cols-[100%_0%] xs:grid-cols-[70%_30%] sm:grid-cols-[50%_50%] md:grid-cols-[40%_60%]',
+      )}
+    >
+      <aside className="overflow-hidden">
         <div className="flex flex-col max-h-screen">
           <div className="flex items-center px-[10px] py-[10px] shadow-[0_2px_2px_rgba(114,114,114,0.25)]">
-            <MenuSvg className="w-10 h-10 mr-[25px] stroke-primary-black/50 flex-auto" />
+            <MenuSvg className="w-10 h-10 mr-[25px] stroke-primary-black/50" />
             <Search />
           </div>
 
-          <ul className="flex flex-col pt-[10px] basis-full overflow-auto">
+          <ul className="flex flex-col pt-[10px] basis-full overflow-x-hidden overflow-y-auto">
             {memoizedChatList}
           </ul>
         </div>
-      </div>
-      <div
-        className={twMerge(
-          'transition-all duration-500 ease-in-out overflow-hidden ' +
-            'lg:basis-4/6 bg-gradient-to-br from-[#8EC5FC] to-[#E0C3FC]',
-          contentIsVisible
-            ? 'basis-full'
-            : 'basis-0 xs:basis-1/3 sm:basis-2/4 md:basis-3/5',
-        )}
-      >
+      </aside>
+      <div className="h-screen overflow-hidden bg-gradient-to-br from-[#8EC5FC] to-[#E0C3FC]">
         <div>Hello World!</div>
       </div>
     </main>
