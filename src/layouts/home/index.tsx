a4c7b839fa5c5f2dfb95301c016bb24b38f33408
Roman Kryzhanivsky <r.krijaniv@gmail.com>
2023-05-08 12:11:06 +0300

    refactor: Home-layout component was deleted from components folder and used in layout/home/

--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-import HomeLayout from '@components/home-layout';
 import Search from '@components/search';
+import UserListItem from '@components/user-list-item';
 import MenuSvg from '@public/icons/menu.svg';
-import UserListItem from 'src/components/user-list-item';
 
 import { chats } from './utils';
 
 const Home: React.FC = () => {
   const [contentIsVisible] = useState(false);
 
+  const memoizedChatList = useMemo(
+    () => chats.map((item) => <UserListItem key={item.id} item={item} />),
+    [],
+  );
+
   return (
-    <HomeLayout
-      contentIsVisible={contentIsVisible}
-      sidebar={
-        <div className="flex flex-col max-h-[100vh]">
+    <main className="w-full min-h-screen flex">
+      <div
+        className={twMerge(
+          'transition-all duration-500 ease-in-out overflow-hidden lg:basis-2/6',
+          !contentIsVisible
+            ? 'basis-full xs:basis-2/3 sm:basis-2/4 md:basis-2/5'
+            : 'basis-0',
+        )}
+      >
+        <div className="flex flex-col max-h-screen">
           <div className="flex items-center px-[10px] py-[10px] shadow-[0_2px_2px_rgba(114,114,114,0.25)]">
-            <MenuSvg className="w-[40px] h-[40px] mr-[25px] stroke-primary-black/50 flex-auto" />
+            <MenuSvg className="w-10 h-10 mr-[25px] stroke-primary-black/50 flex-auto" />
             <Search />
           </div>
 
-          <div className="flex flex-col pt-[10px] basis-[100vh] overflow-auto">
-            {chats.map((item) => (
-              <UserListItem key={item.id} item={item} />
-            ))}
-          </div>
+          <ul className="flex flex-col pt-[10px] basis-full overflow-auto">
+            {memoizedChatList}
+          </ul>
         </div>
-      }
-      content={<div>Hello World!</div>}
-    />
+      </div>
+      <div
+        className={twMerge(
+          'transition-all duration-500 ease-in-out overflow-hidden ' +
+            'lg:basis-4/6 bg-gradient-to-br from-[#8EC5FC] to-[#E0C3FC]',
+          contentIsVisible
+            ? 'basis-full'
+            : 'basis-0 xs:basis-1/3 sm:basis-2/4 md:basis-3/5',
+        )}
+      >
+        <div>Hello World!</div>
+      </div>
+    </main>
   );
 };
 
